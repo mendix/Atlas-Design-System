c15f25321df8c886f29da093c75513ec7c44025c
Diego Antonelli <diego.antonelli@mendix.com>
2021-07-16 14:30:38 +0200

    Migrating from SVN to Github

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Atlas-Design-System
 Atlas Design System App
+
+## Which modules does it generates
+- Atlas Core
+- Atlas Web Content
+- Atlas Native Mobile Content
---
+++ themesource/atlas_designsystem_resources/native/main.js
@@ -0,0 +1,86 @@
+import { unstable_enableLogBox, Dimensions } from "react-native";
+import { spacing, background, border } from "../../../theme/native/custom-variables";
+
+export const tile = {
+  container: {
+    backgroundColor: background.primary,
+    borderColor: background.secondary,
+    borderWidth: 1,
+    padding: spacing.regular,
+
+  },
+};
+
+export const tileHeader = {
+  container: {
+    marginBottom: spacing.regular,
+  },
+};
+
+export const stretchedVideo = {
+  container: {
+    maxHeight: "100%",
+  },
+  video: {
+    height: 200,
+    width: 200,
+  },
+};
+
+export const quarterScreenHeightContainer = {
+  container: {
+    height: Dimensions.get("window").height / 4,
+  },
+};
+
+export const halfScreenHeightContainer = {
+  container: {
+    height: Dimensions.get("window").height / 2,
+  },
+};
+
+export const backgroundColorBlack = {
+  container: {
+    backgroundColor: "#0A1325",
+  },
+};
+
+export const containerPlaceHolder = {
+  container: {
+    height: 100,
+    padding: spacing.smaller,
+    backgroundColor: background.secondary,
+    borderColor: border.color,
+    borderWidth: 1,
+  },
+};
+
+//
+// Wrappers
+export const width50PercentWrapper = {
+  container: {
+    width: "50%",
+    padding: spacing.small,
+  },
+};
+
+export const cardThumbnail = {
+  container: {
+    backgroundColor: "#7d92ef",
+    height: 128,
+    padding: spacing.regular,
+    borderTopLeftRadius: border.radiusLarge,
+    borderTopRightRadius: border.radiusLarge,
+  },
+  image: {
+    resizeMode: 'contain',
+  }
+}
+
+export const maxes = {
+  container: {
+    maxWidth: 300,
+    maxHeight: 500,
+  },
+
+};
