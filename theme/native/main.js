1fcd7b49eef4f2ed7fdde253d474dd66d17d29d7
Jordan Hunt <jordancolehunt@gmail.com>
2021-07-23 13:32:22 +0000

    - delete GroupBox (replaced by Accordion) - add Line Chart example page

--- theme/native/main.js
+++ theme/native/main.js
@@ -1,1 +1,30 @@
-export {};
+const com_mendix_widget_native_linechart_LineChart = {
+    lines: {
+        customLineStyles: {
+            line1: {
+                line: {
+                    lineColor: "orange"
+                },
+                markers: {
+                    symbol: "diamond",
+                    backgroundColoer: "red",
+                    size: 10
+                }
+            },
+            line2: {
+                line: {
+                    lineColor: "green"
+                },
+                markers: {
+                    symbol: "square",
+                    backgroundColoer: "yellow",
+                    size: 10
+                }
+            }
+        }
+    }
+}
+
+export {
+    com_mendix_widget_native_linechart_LineChart
+};
