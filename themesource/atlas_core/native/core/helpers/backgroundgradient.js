d149ed54205a6eceb95633751f79b6e62093def9
UI Content <widgets@mendix.com>
2022-05-10 16:10:53 +0000

    Updated widgets and styling

---
+++ themesource/atlas_core/native/core/helpers/backgroundgradient.js
@@ -0,0 +1,12 @@
+/*
+
+DISCLAIMER:
+Do not change this file because it is core styling.
+Customizing core files will make updating Atlas much more difficult in the future.
+To customize any core styling, copy the part you want to customize to styles/native/app/ so the core styling is overwritten.
+==========================================================================
+*/
+// Background Gradient properties
+export const backgroundGradient = {
+    container: {}
+};
